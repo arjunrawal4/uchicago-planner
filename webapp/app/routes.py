795b785de59553d10dd67ea8d4691cb50226de6e
arjunrawal4 <arjunrawal4@gmail.com>
2018-02-10 12:33:10 -0600

    modifying gitignore

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,7 +1,14 @@
-from app import app
+from app import app, db
+import logging
 from flask import Flask, render_template, request
+from app.models import User
+from sqlalchemy import Table, Column, MetaData, DateTime
+from sqlalchemy.orm import joinedload
+log = logging.getLogger("webapp")
 
 @app.route('/')
 @app.route('/templates/index')
 def index():
-    return render_template('index.html')
+    users = db.session.query(User)
+    log.info(users)
+    return render_template('index.html', users=users)
